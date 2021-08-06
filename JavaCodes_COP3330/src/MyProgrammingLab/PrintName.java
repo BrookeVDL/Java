a6d3f05ad12b277600efa62ee48e6298c8de116a
BrookeVDL <88516951+BrookeVDL@users.noreply.github.com>
2021-08-06 09:43:35 -0400

    Print Name Program

---
+++ JavaCodes_COP3330/src/MyProgrammingLab/PrintName.java
@@ -0,0 +1,16 @@
+package MyProgrammingLab;
+
+//Author: Brooke VanDerLinden
+//Date: 5/18/2021
+//Course: COP 3330
+//Exercise: 20506
+//Writing a Complete main method to print your full name
+//Last name, First Name
+
+public class PrintName {
+	
+//Main method to start the Java application
+	public static void main (String[] args){
+	System.out.println("Gershwin, George");
+		  }
+	}
